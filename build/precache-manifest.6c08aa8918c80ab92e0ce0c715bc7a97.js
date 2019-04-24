620551d0635f638f4a10cb77d9bdb8eddfd16393
Murat Mavlidov <mmavlidov96@mail.ru>
2019-04-24 12:37:12 +0300

    Drawing and setting menu, dimming the screen, preparing for routing

---
+++ build/precache-manifest.6c08aa8918c80ab92e0ce0c715bc7a97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "414f139d721321f78e9f",
+    "url": "/static/css/main.de4cf9a7.chunk.css"
+  },
+  {
+    "revision": "414f139d721321f78e9f",
+    "url": "/static/js/main.e7b2fe7e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8e5a0f534a2ca840552c",
+    "url": "/static/js/2.40ab9b41.chunk.js"
+  },
+  {
+    "revision": "f02c8f6c06856d42600333547a010878",
+    "url": "/index.html"
+  }
+];
